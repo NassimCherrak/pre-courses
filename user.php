a077b632d489f3bc3c099cf357c5664a8b02251a
NassimCherrak <37450061+NassimCherrak@users.noreply.github.com>
2018-04-19 06:17:16 -0400

    start
    register courses

---
+++ user.php
@@ -0,0 +1,28 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "root", "", "angusql");
+
+$user = 1;
+
+$sql = "SELECT id, name, email, date, gender, status, bio FROM user WHERE id=". $user;
+
+$result = $conn->query($sql);
+
+$outp = "";
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {$outp .= ",";}
+    $outp .= '{"id":"'  . $rs["id"] . '",';
+    $outp .= '"name":"'. $rs["name"] . '",';
+    $outp .= '"email":"'. $rs["email"] . '",';
+    $outp .= '"date":"'. $rs["date"] . '",';
+    $outp .= '"gender":"'. $rs["gender"] . '",';
+    $outp .= '"status":"'. $rs["status"] . '",';
+    $outp .= '"bio":"'. $rs["bio"] . '"}';
+}
+$outp ='{"records":['.$outp.']}';
+$conn->close();
+
+echo($outp);
+?>
